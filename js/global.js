ba1538e88120ccdd63902e6069d497b6a686ad45
Thiago Duarte de Oliveira Braga <thiago.duarte@a.ficr.edu.br>
2021-11-04 22:05:12 -0300

    arquivos da aula 02

---
+++ js/global.js
@@ -0,0 +1,6 @@
+const cardNovaLista = document.getElementById('cardNovaLista')
+const btnNovaLista = document.getElementById('btnNovaLista')
+
+btnNovaLista.addEventListener('click', function(){
+    cardNovaLista.style.display = 'block'
+})
