126e0ccd8935850ed1d1bdf217b24d2189abee05
Thiago Duarte de Oliveira Braga <thiago.duarte@a.ficr.edu.br>
2021-11-11 22:08:04 -0300

    aula1111221

--- js/global.js
+++ js/global.js
@@ -1,6 +1,29 @@
 const cardNovaLista = document.getElementById('cardNovaLista')
 const btnNovaLista = document.getElementById('btnNovaLista')
+const formNovaLista = document.getElementById('novaLista')
+
+
+const listas = []
 
 btnNovaLista.addEventListener('click', function(){
     cardNovaLista.style.display = 'block'
+})
+
+formNovaLista.addEventListener('submit', function(event){
+    event.preventDefault()
+    const inputTitulo = document.querySelector('input[name="tituloLista"]')
+    if (inputTitulo.value != ''){
+        const novaLista = {
+            id: listas.length + 1,
+            titulo: inputTitulo.value
+        }
+        inputTitulo.value = ''
+        listas.push(novaLista)
+        window.localStorage.listas = JSON.stringify(listas)
+    }
+    console.log(listas)
+})
+
+formNovaLista.addEventListener('reset', function(event){
+    cardNovaLista.style.display = 'none'
 })
